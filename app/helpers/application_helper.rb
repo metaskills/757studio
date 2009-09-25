17b9c28aadc943b3b9a99a39356ebc3ec9d398b9
Ken Collins <ken@metaskills.net>
2009-09-25 15:44:21 -0400

    Making the navigation cross-browser positioned, background, text shadow.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
   def top_nav(page_name)
     page_param = page_name.downcase.gsub(/\s/,'_')
     link_class = current_page == page_param ? 'active' : ''
-    content_tag :li do
-      link_to page_name, site_path(page_param), :class => link_class
+    content_tag :li, :class => link_class do
+      link_to page_name, site_path(page_param)
     end
   end
   
